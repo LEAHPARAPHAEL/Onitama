0826604a9f47357b07403d558bf59484765f8f0c
Oscar Garnier <ogpourcommander@gmail.com>
2026-03-30 16:57:11 +0200

    skinny is ready to run

--- makefile
+++ makefile
@@ -1,19 +1,19 @@
 ## For remote work
 
-CURRENT_RUN := se_net_6_96_no_aug
-TIME_LIMIT := 31:00:00
+JOB ?= se_net_6_96_no_aug
+TIME_LIMIT ?= 31:00:00
 
 generate_job_script:
-	python remote/generate_script.py --job ${CURRENT_RUN} --time ${TIME_LIMIT}
+	python remote/generate_script.py --job ${JOB} --time ${TIME_LIMIT}
 
 launch_job:
-	sbatch remote/scripts/${CURRENT_RUN}.sh
+	sbatch remote/scripts/${JOB}.sh
 
 check_job:
 	squeue
 
 view_job_output:
-	cat logs/${CURRENT_RUN}.out
+	cat logs/${JOB}.out
 
 play:
 	python -m gui.gui
---
+++ remote/scripts/skinny_se_net_6_96.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -p mesonet 
+#SBATCH --account=m25146
+#SBATCH --job-name=skinny_se_net_6_96
+#SBATCH --output=logs/skinny_se_net_6_96.out
+#SBATCH --error=logs/skinny_se_net_6_96.out
+#SBATCH --time=31:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:1
+#SBATCH --mail-user=oscar.garnier@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+# Activate virtual environment
+source .venv/bin/activate
+
+# Run the training script
+python -m train.end_to_end -c skinny_se_net_6_96.yaml
